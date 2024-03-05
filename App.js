731dc116f9009ab30e397ab3bca068608adec43a
Geovanna Menezes <123017757+geovannamenezes@users.noreply.github.com>
2024-03-05 17:11:45 -0300

    alteração

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto TG - Invest</Text>
+      <Text>Projeto TG - Invest geovannas</Text>
       <StatusBar style="auto" />
     </View>
   );

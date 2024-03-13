3602d6c4d07ba4de9c231d09b1bf7713da5a8eae
Geovanna Menezes <123017757+geovannamenezes@users.noreply.github.com>
2024-03-13 17:45:54 -0300

    add icones expo

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import Home from "./src/pages/Home/Home";
+// documentação icones, fontes,  https://docs.expo.dev/guides/icons/
+// icones https://icons.expo.fyi/Index
 
 export default function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Pressable, StatusBar, StyleSheet, Text } from 'react-native'
 import { View } from 'react-native-web'
 import styles from '../../assets/scss/index.scss'
-// import 'material-icons/iconfont/material-icons.css';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 :  64;
 
@@ -11,7 +11,7 @@ export default function Header({ name }) {
         <View style={styles.containerHeader}>
             <View style={styles.content}>
                 <Pressable activeOpacity={0.9}>
-                    <Text>icone</Text>
+                    <Ionicons name="person-circle-sharp" size={24} color="white" />
                 </Pressable>
                 <Text style={styles.username}>Olá, {name}</Text>
             </View>

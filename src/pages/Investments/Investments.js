b47b8c2db87fc6db88937bb5dfb34e822a18471b
Geovanna Menezes <geovanna.menezes25@gmail.com>
2024-06-23 21:24:47 -0300

    add taxa selic

--- src/pages/Investments/Investments.js
+++ src/pages/Investments/Investments.js
@@ -3,12 +3,30 @@ import Header from '../../components/Header/Header';
 import styles from '../../assets/scss/index.scss'
 import Footer from '../../components/Footer/Footer';
 import typo from '../../assets/scss/typography.scss'
+import { get } from '../../services/Services';
+import { useEffect, useState } from 'react';
 
 export default function Investments({ navigation }) {
+
+  const [selic, setSelic] = useState([]);
+
+  useEffect(() => {
+    getSelic()
+  }, []);
+
+  function getSelic() {
+    get("https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados/ultimos/1?formato=json")
+    .then((data) => {
+      setSelic(data)
+      console.log(data)
+    })
+  }
+
   return (
     <View style={styles.container}>
         <Header navigation={navigation} />
         <Text style={typo.title_alt}>Investimentos</Text>
+        <Text>Taxa Selic atual {selic[0]?.valor} ({selic[0]?.data})</Text>
         <Footer navigation={navigation} />
     </View>
   );
--- src/services/Services.js
+++ src/services/Services.js
@@ -1,5 +1,5 @@
 
-export function post({ url, body }) {
+export function post(url, body) {
     fetch(url, {
         method: 'POST',
         headers: {
@@ -13,9 +13,9 @@ export function post({ url, body }) {
     .catch((error) => console.error(error));
 }
 
-export function get({ url }) {
-    fetch(urk)
-    .then((response) => { return response.json() })
-    .then((json) => console.log(json))
+export async function get(url) {
+    return fetch(url)
+    .then((response) => response.json())
+    .then((json) => { return json })
     .catch((error) => console.error(error));
 }

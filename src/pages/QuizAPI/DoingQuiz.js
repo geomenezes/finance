07d98a7e2e8aef17ef1dbc0618a4604ff33fdfa4
geomedeiros <geovannamedeiros2309@gmail.com>
2024-09-01 16:55:00 -0300

    Corrigindo caquinha
    Mexi sem querer no código do quiz

--- src/pages/QuizAPI/DoingQuiz.js
+++ src/pages/QuizAPI/DoingQuiz.js
@@ -1,33 +1,113 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { PieChart } from 'react-native-svg-charts';
+import { Text, View } from "react-native";
+import { questions, resultQuiz } from "./Info";
+import ButtonChoice from "../../components/Buttons/ButtonChoice";
+import { useState } from "react";
+import Header from "../../components/Header/Header";
+import ActionButton from "../../components/Buttons/Action";
+import typo from '../../assets/scss/typography.scss'
 
-const sampleData = [
-  { key: 1, amount: 50, svg: { fill: '#4CAF50' } },
-  { key: 2, amount: 30, svg: { fill: '#FFC107' } },
-  { key: 3, amount: 20, svg: { fill: '#FF5722' } },
-];
+export function DoingQuiz({ navigation }) {
 
-export default function RelatorioGastos() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Relatório de Gastos</Text>
-      <PieChart style={{ height: 200, marginVertical: 20 }} data={sampleData} innerRadius="70%" />
-    </View>
-  );
+    const [step, setStep] = useState(0);
+    const [qtQuestions, setQtQuestions] = useState(questions.length);
+    const [points, setPoints] = useState(0);
+    const [seeNext, setSeeNext] = useState(false);
+    const [currentPoint, setCurrentPoint] = useState(0);
+    const [reference, setReference] = useState([]);
+
+    function nextQuestion() {
+        setStep(prev => prev + 1)
+        setSeeNext(false)
+        cleanButtons(questions[step].alternatives, -1)
+    }
+
+    function countPoints(point) {
+        if(reference.length > 0) {
+            let ref = ([...reference, point])
+
+            if(ref.length > ref[0]) {
+                var count = 0;
+                ref.map((e, key) => {
+                    if(key !== 0) {
+                        count += e;
+                    }
+                })
+                
+                if(count < 2) {
+                    setPoints(prev => prev + 1)
+                } else if(count < 5) {
+                    setPoints(prev => prev + 2)
+                } else setPoints(prev => prev + 4)
+
+            } 
+            else setReference(ref)
+        }  else setPoints(prev => prev + point)
+    }
+
+    function result() {
+        let profile;
+        if(points < 15) {
+            profile = resultQuiz.find(e => e.id == "conservative")
+        } else if(points < 36) {
+            profile = resultQuiz.find(e => e.id == "moderate")
+        } else {
+            profile = resultQuiz.find(e => e.id == "audacious")
+        }
+
+        navigation.navigate("ResultQuiz", { profile })
+    }
+
+    function changeColor(id, others) {
+        let element = document.getElementById("button-color-" + id);
+        if (element != null) {
+          element.style.backgroundColor = "#0DA980";
+        }
+        cleanButtons(others, id)
+    }
+
+    function cleanButtons(data, id) {
+        data.map((e, key) => {
+            if(key !== id) {
+                let element = document.getElementById("button-color-" + key);
+                if (element != null) {
+                element.style.backgroundColor = "#dbd7d7";
+                }
+            }
+        })
+    }
+
+    return (
+        <View>
+            <Header navigation={navigation} />
+            {
+                <>
+                    <Text style={typo.title}>{questions[step].question}</Text>
+                    {questions[step].alternatives.map((e, key) => (
+                        <ButtonChoice 
+                            key={key}
+                            id={"button-color-" + key}
+                            label={e.label} 
+                            onClick={() => {
+                                setCurrentPoint(e.value)
+                                setSeeNext(true)
+                                changeColor(key, questions[step].alternatives)
+                                if(questions[step]?.reference)
+                                    setReference([questions[step]?.reference])
+                            }}
+                        />
+                    ))}
+                </>
+            }
+            {(seeNext && (step + 1 < qtQuestions)) && 
+                <ActionButton label="Próximo" onClick={() => {
+                    countPoints(currentPoint)
+                    nextQuestion()
+                }} />}
+            {(seeNext && !(step + 1 < qtQuestions)) && 
+                <ActionButton label="Enviar" onClick={() => {
+                    countPoints(currentPoint)
+                    result()
+                }} />}
+        </View>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#F5F5F5',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#004D40',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-});

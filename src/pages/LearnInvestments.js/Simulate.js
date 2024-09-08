be2671abfba8d68b8f82176babd938dbc9349de3
Geovanna Menezes <89481951+geomenezes@users.noreply.github.com>
2024-09-07 21:40:10 -0300

    tentando add input date

--- src/pages/LearnInvestments.js/Simulate.js
+++ src/pages/LearnInvestments.js/Simulate.js
@@ -1,10 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Simulate({ navigation }) {
   
+
+
     return (
       <View>
-          <Text>Simular</Text>          
+          <Text>Simular</Text>
+            <View>
+                <RNDateTimePicker mode="time" />
+            </View>
       </View>
     );
   }
